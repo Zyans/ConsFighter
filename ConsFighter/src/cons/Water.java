ee2eec1585ceb97d90c657baf5bc6fd0e96ad325
Lukas Tassanyi <tassanyis@web.de>
2014-01-11 12:40:36 +0100

    A3.10_14_01_11_L

--- ConsFighter/src/cons/Water.java
+++
@@ -1,8 +0,0 @@
-package cons;
-
-public class Water extends Field {
-	Water(Texture texture, int x, int y, int height, boolean addToEditor) {
-        super(texture, x, y, height, addToEditor);
-        setCanGoThrough(false);
-    }
-}
